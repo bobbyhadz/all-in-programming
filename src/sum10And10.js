035260f874eb6b5b09667b7cb86d1ca6a93b37f9
Borislav Hadzhiev <opmftw@gmail.com>
2022-08-04 19:03:24 +0300

    add sum10And10function

---
+++ src/sum10And10.js
@@ -0,0 +1,3 @@
+export function sum10And10() {
+  return 10 + 10;
+}
---
+++ test/sum10And10.test.js
@@ -0,0 +1,5 @@
+import {sum10And10} from '../src/sum10And10';
+
+it('returns 20 when adding 10 and 10', () => {
+  expect(sum10And10()).toBe(20);
+});
